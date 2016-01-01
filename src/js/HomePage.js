00a0e52c4c0dab433701767f0b6c0a92b8a25d68
Maxwel Dsouza <maxellusionist@gmail.com>
2016-01-02 02:00:05 +0530

    minor cleanup

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -26,35 +26,31 @@ var HomePage = React.createClass({
                     <div className='top-bar-left'>
                         <ul className='dropdown menu' data-dropdown-menu>
                             <li className='menu-text'>Mentions</li>
-                            <li className='has-submenu'>
-                                <a href='#'>One</a>
-                                <ul className='submenu menu vertical' data-submenu>
-                                    <li><a href='#'>One</a></li>
-                                    <li><a href='#'>Two</a></li>
-                                    <li><a href='#'>Three</a></li>
-                                </ul>
-                            </li>
-                            <li><a href='#'>Two</a></li>
-                            <li><a href='#'>Three</a></li>
+                            <li><a href='#'>New</a></li>
+                            <li><a href='#'>Popular</a></li>
                         </ul>
                     </div>
                     <div className='top-bar-right'>
                         <ul className='menu'>
                             <li><input type='search' placeholder='Search' /></li>
                             <li><button type='button' className='button'>Search</button></li>
+                            <li><a href='#'>Login</a></li>
+                            <li><a href='#'>Signup</a></li>
                         </ul>
                     </div>
                 </div>
                 <div className='row'>
-                    <h1>Mentions</h1>
-                </div>
-                <div className='row'>
-                    <div className='small-2 columns'>2 columns</div>
-                    <div className='small-10 columns'>10 columns</div>
-                </div>
-                <div className='row'>
-                    <div className='small-3 columns'>3 columns</div>
-                    <div className='small-9 columns'>9 columns</div>
+                    <div className='small-12 columns'>
+                        <h2>Top Mentions</h2>
+                        <div className='row'>
+                            <div className='small-12 columns'>
+                                Richard Dawkins mentioned V S Ramachandran
+                            </div>
+                            <div className='small-12 columns'>
+                                "The marco polo of neuroscience"
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </span>
         );
