402de4f897493ec7aa63856ecd545d416709486f
Maxwel Dsouza <maxellusionist@gmail.com>
2016-02-23 01:31:58 +0530

    update react

--- src/js/MainComponent.js
+++ src/js/MainComponent.js
@@ -1,5 +1,5 @@
 /* The main component is used both on the server and the client
- * side. It takes only one prop 'path'. */
+* side. It takes only one prop 'path'. */
 
 var React = require('react');
 
@@ -16,6 +16,7 @@ var ProfilePage = require('./ProfilePage');
 var Signup = require('./Signup');
 var Spinner = require('./Spinner');
 var ThingPage = require('./ThingPage');
+var Sidebar = require('react-sidebar');
 
 var store = require('store');
 
@@ -24,55 +25,26 @@ var MainComponent = React.createClass({
         component: React.PropTypes.string.isRequired,
         data: React.PropTypes.object.isRequired
     },
-    getInitialState: function () {
-        /* this is the entire app state
-         * it will persist across browser tabs since it is stored
-         * in localstorage
-         * also any changes to appstate will reflect in other tabs
-         * immediately */
-        var appstate = store.get('appstate');
-        if (typeof appstate === 'undefined') {
-            appstate = {};
-        }
-        appstate.shortlistHidden = true;
-        appstate.sidebar = false;
-        /* we need to check for properties since we may be using an older
-         * version of object from localstorage */
-        if (typeof appstate.shortlist === 'undefined') {
-            appstate.shortlist = {};
-            if (typeof appstate.shortlist.mobiles === 'undefined') {
-                appstate.shortlist.mobiles = [];
-            }
-        }
-        return appstate;
-    },
-    componentDidMount: function () {
-        window.addEventListener('storage', this.onLocalStorageChanged);
-    },
-    componentWillReceiveProps () {
-        this.setState({
+    getInitialState () {
+        return {
             sidebar: false
+        };
+    },
+    onSidebarOpen (open) {
+        this.setState({
+            sidebar: true
         });
     },
-    componentDidUpdate: function () {
-        store.set('appstate', this.state);
-    },
-    componentWillUnMount: function () {
-        window.removeEventListener('storage', this.onLocalStorageChanged);
-    },
-    onLocalStorageChanged: function () {
-        var appstate = store.get('appstate');
-        this.setState(appstate);
-    },
-    render: function () {
+    render () {
         var Component = require('./' + this.props.component);
+        var sidebarContent = <b>Sidebar content</b>;
         return (
             <div>
                 <Spinner />
                 <Component
                     data={this.props.data}
                     path={this.props.path}
-                />
+                    />
             </div>
         );
     }
