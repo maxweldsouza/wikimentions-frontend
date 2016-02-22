9134386eda5c6c7bfb10c3861543c45716d0b614
Maxwel Dsouza <maxellusionist@gmail.com>
2016-02-22 12:35:51 +0530

    mentions icon

--- src/js/Mention.js
+++ src/js/Mention.js
@@ -3,10 +3,16 @@ var React = require('react');
 var Mention = React.createClass({
     render () {
         var path;
+        var icon;
+        var description;
         if (this.props.type === 'book') {
             path = '/books/' + this.props.id + '/' + this.props.slug;
+            icon = 'ion-ios-book';
+            description = 'Book';
         } else if (this.props.type === 'person') {
             path = '/pages/' + this.props.id + '/' + this.props.slug;
+            icon = 'ion-person';
+            description = this.props.description;
         }
         return (
             <div className='row mention-block'>
@@ -17,7 +23,9 @@ var Mention = React.createClass({
                     <a href={'/mentions/' + this.props.id + '/edit'}>Edit</a>
                 </div>
                 <div className='small-12 columns mention-short-description'>
-                    {this.props.description}
+                    <span className='mention-icon'>
+                        <span className={icon}/>
+                    </span>{description}
                 </div>
                 <div className='small-12 columns mention-quote'>
                     {this.props.quote}
