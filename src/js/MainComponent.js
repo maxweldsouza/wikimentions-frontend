653301565dff6a9ad3ef60b9ad8e733d42e51b7d
Maxwel Dsouza <maxellusionist@gmail.com>
2016-02-23 12:22:57 +0530

    sidebar color and search icon

--- src/js/MainComponent.js
+++ src/js/MainComponent.js
@@ -41,8 +41,8 @@ var MainComponent = React.createClass({
             <div>
                 <div id="outer-container">
                     <Menu isOpen={false} pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-                        <a id="home" className="menu-item" href="/">
-                            <span className='ion-android-home menu-item-icon'/>Home</a>
+                        <a id="home" className="menu-item" href="/"><span className='ion-android-home menu-item-icon'/>Home</a>
+                        <a id="about" className="menu-item" href="/create"><span className='ion-android-search menu-item-icon'/>Search</a>
                         <a id="about" className="menu-item" href="/create"><span className='ion-android-create menu-item-icon'/>Create Page</a>
                         <a id="contact" className="menu-item" href="/contact"><span className='ion-android-contact menu-item-icon'/>Contact</a>
                     </Menu>
