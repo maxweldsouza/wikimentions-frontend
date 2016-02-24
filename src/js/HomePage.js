57c6f2e5ba8cbde0cee2722a36e91eadf2d19bc1
Maxwel Dsouza <maxellusionist@gmail.com>
2016-02-25 02:20:09 +0530

    minor edit homepage

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -37,7 +37,7 @@ var HomePage = React.createClass({
                         <h2>Top Mentions</h2>
                         <div className='row'>
                             <div className='small-12 columns'>
-                                <a href='/pages/18/richard-dawkins'>Richard Dawkins</a> mentioned <a href='/books/phantoms-in-the-brain'>Phantoms in the brain</a>
+                                <a href='/pages/18/richard-dawkins'>Richard Dawkins</a> mentioned <a href='/books/2/phantoms-in-the-brain'>Phantoms in the brain</a>
                             </div>
                             <div className='small-12 columns'>
                                 "The marco polo of neuroscience"
