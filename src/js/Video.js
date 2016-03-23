186ba4cc8ff0791f378d7b90e86c04de7a8f4b60
Maxwel Dsouza <maxellusionist@gmail.com>
2016-03-24 01:27:36 +0530

    fix video format

--- src/js/Video.js
+++ src/js/Video.js
@@ -3,12 +3,14 @@ var React = require('react');
 var Video = React.createClass({
     render () {
         return (
-            <div className='small-12 columns'>
-                <div className='shrink columns'>
-                    <img className="" src="/assets/video.png" alt=""/>
-                </div>
-                <div className='columns'>
-                    <a href={'/videos/' + this.props.id + '/' + this.props.slug}>{this.props.title}</a>
+            <div className='small-12 columns video-block'>
+                <div className='row'>
+                    <div className='shrink columns'>
+                        <img className="" src="/assets/video.png" alt=""/>
+                    </div>
+                    <div className='columns'>
+                        <a className='video-title' href={'/videos/' + this.props.id + '/' + this.props.slug}>{this.props.title}</a>
+                    </div>
                 </div>
             </div>
         );
