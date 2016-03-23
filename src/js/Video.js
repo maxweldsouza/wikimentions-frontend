9c27dd59eb323f931ccc8a63299f44ac2af29fd5
Maxwel Dsouza <maxellusionist@gmail.com>
2016-03-24 00:29:21 +0530

    video thumbnail, build not working

--- src/js/Video.js
+++ src/js/Video.js
@@ -4,7 +4,12 @@ var Video = React.createClass({
     render () {
         return (
             <div className='small-12 columns'>
-                <a href={'/videos/' + this.props.id + '/' + this.props.slug}>{this.props.title}</a>
+                <div className='shrink columns'>
+                    <img className="" src="/assets/video.png" alt=""/>
+                </div>
+                <div className='columns'>
+                    <a href={'/videos/' + this.props.id + '/' + this.props.slug}>{this.props.title}</a>
+                </div>
             </div>
         );
     }
