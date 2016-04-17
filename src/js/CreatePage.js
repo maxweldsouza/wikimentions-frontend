c41343059bde8c0d0a4433ec51b3d64e9ea3d577
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-17 21:21:35 +0530

    create page ajax, error message

--- src/js/CreatePage.js
+++ src/js/CreatePage.js
@@ -5,7 +5,8 @@ var Login = require('./Login');
 var Signup = require('./Signup');
 var cookies = require('browser-cookies');
 var ButtonSelect = require('./ButtonSelect');
-var Xsrf = require('./Xsrf');
+var Notification = require('./Notification');
+var requests = require('superagent');
 
 var HomePage = React.createClass({
     statics: {
@@ -17,13 +18,58 @@ var HomePage = React.createClass({
     },
     getInitialState () {
         return {
-            type: 'person'
+            type: 'person',
+            title: '',
+            description: '',
+            isbn: '',
+            submiting: false,
+            error: false,
+            message: ''
         };
     },
     onChangeType (x) {
         this.setState({
             type: x
         });
+    },
+    onChangeText (e) {
+        var temp = {}
+        temp[e.target.name] = e.target.value;
+        this.setState(temp);
+    },
+    onCloseError () {
+        this.setState({
+            error: false,
+            message: ''
+        });
+    },
+    onSubmit () {
+        this.setState({
+            submiting: true
+        });
+        requests
+        .post('/api/v1/thing')
+        .type('form')
+        .send({
+            title: this.state.title,
+            description: this.state.description,
+            isbn: this.state.isbn,
+            action: 'create',
+            _xsrf: cookies.get('_xsrf')
+        })
+        .end((err, res) => {
+            this.setState({
+                submiting: false
+            });
+            if (err.status) {
+                this.setState({
+                    error: true,
+                    message: res.body.message
+                });
+            } else {
+                Mentions.route(window.location.pathname + window.location.search);
+            }
+        })
     },
     render () {
         var options = [{name: 'Book', value: 'book'},
@@ -45,11 +91,10 @@ var HomePage = React.createClass({
                 <div className='row page-body align-center'>
                     <div className='small-12 large-8 columns'>
                         <form action='/api/v1/thing' method='post'>
-                            <input type='hidden' name='action' value='create'/>
-                            <Xsrf/>
                             <h1 className='page-title'>Create Page</h1>
-                            <input type='text' name='title' placeholder='Title' />
-                            <input type='text' name='description' placeholder='Description' />
+                            <Notification level='alert' message={this.state.message} showing={this.state.error} onClose={this.onCloseError} closeable/>
+                            <input type='text' name='title' placeholder='Title' value={this.state.title} onChange={this.onChangeText} required/>
+                            <input type='text' name='description' placeholder='Description (Optional)' value={this.state.description}  onChange={this.onChangeText}/>
                             <div className='row'>
                                 <div className='small-12 medium-6 columns'>
                                     <label>Type
@@ -60,13 +105,13 @@ var HomePage = React.createClass({
                                     </label>
                                 </div>
                                 <div className='small-12 medium-6 columns'>
-                                    {this.state.type === 'book' ? <input type='text' name='isbn' placeholder='ISBN' /> : null}
+                                    {this.state.type === 'book' ? <input type='text' name='isbn' placeholder='ISBN' value={this.state.isbn} onChange={this.onChangeText}/> : null}
                                 </div>
                             </div>
                             <label htmlFor="exampleFileUpload" className="button">Upload Image</label>
                             <input type="file" id="exampleFileUpload" className="show-for-sr"/>
                             <div>
-                                <button type='submit' className='success button'>Create</button>
+                                {this.state.submiting ? <button type='button' className='success button disabled' onClick={this.onSubmit}>Create</button> : <button type='button' className='success button' onClick={this.onSubmit}>Create</button>}
                             </div>
                         </form>
                     </div>
