637c0bcc20d19f011fe6b3a01ba83ab8304156ab
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-19 16:38:57 +0530

    directly add new book

--- src/js/AddBookNew.js
+++ src/js/AddBookNew.js
@@ -1,12 +1,82 @@
 var React = require('react');
+var Notification = require('./Notification');
+var SubmitButton = require('./SubmitButton');
+var cookies = require('browser-cookies');
+var requests = require('superagent');
 
 var AddBookNew = React.createClass({
+    getInitialState () {
+        return {
+            title: '',
+            description: '',
+            error: false,
+            message: ''
+        };
+    },
+    onChangeText (e) {
+        var temp = {};
+        temp[e.target.name] = e.target.value;
+        this.setState(temp);
+    },
+    onCloseError () {
+        this.setState({
+            error: false,
+            message: ''
+        });
+    },
+    onSubmit () {
+        this.setState({
+            submitting: true
+        });
+        requests
+        .post('/api/v1/thing')
+        .type('form')
+        .send({
+            title: this.state.title,
+            description: this.state.description,
+            type: 'book',
+            action: 'create',
+            _xsrf: cookies.get('_xsrf')
+        })
+        .end((err, res) => {
+            if (err && err.status) {
+                this.setState({
+                    error: true,
+                    message: res.body.message
+                });
+            } else {
+                requests
+                .post('/api/v1/thing/' + this.props.id + '/books')
+                .type('form')
+                .send({
+                    book_id: res.body.id,
+                    _xsrf: cookies.get('_xsrf')
+                })
+                .end((err, res) => {
+                    this.setState({
+                        submitting: false,
+                        title: '',
+                        description: '' 
+                    });
+                    if (err && err.status) {
+                        this.setState({
+                            error: true,
+                            message: res.body.message
+                        });
+                    } else {
+                        Mentions.route(window.location.pathname + window.location.search);
+                    }
+                });
+            }
+        });
+    },
     render () {
         return (
             <form method='post' action={'/api/v1/thing/' + this.props.id + '/books'}>
-                <input type='text' name='title' placeholder='Title'/>
-                <input type='text' name='description' placeholder='Short Description' />
-                <button type='button' className='button' onClick={this.onSubmit}>Save</button>
+                <Notification level='alert' message={this.state.message} showing={this.state.error} onClose={this.onCloseError} closeable/>
+                <input type='text' name='title' placeholder='Title' value={this.state.title} onChange={this.onChangeText} required/>
+                <input type='text' name='description' placeholder='Description (Optional)' value={this.state.description} onChange={this.onChangeText}/>
+                <SubmitButton title='Create' submitting={this.state.submitting} onSubmit={this.onSubmit}/>
             </form>
         );
     }
