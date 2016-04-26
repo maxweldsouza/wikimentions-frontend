ade515a3b67aa15f72911fbfe0ddddc99676c3f9
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-27 00:15:23 +0530

    button group fix styling

--- src/js/ButtonSelect.js
+++ src/js/ButtonSelect.js
@@ -20,15 +20,17 @@ var ButtonSelect = React.createClass({
                     name={this.props.name}
                     value={this.state.selected}
                 />
-                {this.props.options.map((x) => {
-                    var cls;
-                    if (this.state.selected === x.value) {
-                        cls = 'button small primary';
-                    } else {
-                        cls = 'button small secondary hollow';
-                    }
-                    return <button type='button' className={cls} onClick={this.onChangeSelected.bind(null, x.value)} key={x.value}>{x.name}</button>;
-                })}
+                <div className='small button-group'>
+                    {this.props.options.map((x) => {
+                        var cls;
+                        if (this.state.selected === x.value) {
+                            cls = 'button primary';
+                        } else {
+                            cls = 'button secondary hollow';
+                        }
+                        return <button type='button' className={cls} onClick={this.onChangeSelected.bind(null, x.value)} key={x.value}>{x.name}</button>;
+                    })}
+                </div>
             </div>
         );
     }
