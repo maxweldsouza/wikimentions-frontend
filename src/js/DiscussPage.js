928456400a40e11c14fcecb1c1c72405df41122e
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-27 01:31:41 +0530

    fix discuss page no data

--- src/js/DiscussPage.js
+++ src/js/DiscussPage.js
@@ -43,12 +43,10 @@ var DiscussPage = React.createClass({
                 })}
             </div>;
         } else {
-            main = <div className='row'>
-                <div className='small-12 columns'>
+            main = <div className='small-12 columns'>
                     <div className='discuss-empty callout primary'>
                     There are no discussions here. You can start one !
                     </div>
-                </div>
             </div>;
         }
         return (
