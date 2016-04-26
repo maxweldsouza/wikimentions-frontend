eb461a805220ad87febdb37eb610586f8e80d5b3
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-26 20:38:23 +0530

    fix minor bug

--- src/js/Video.js
+++ src/js/Video.js
@@ -5,10 +5,10 @@ var Video = React.createClass({
     render () {
         var imagedata, image;
         if (this.props.images.length > 0) {
-            imagedata = this.props.length.images[0];
+            imagedata = this.props.images[0];
             image = <Image id={this.props.id} md5={imagedata.md5} width={imagedata.width} height={imagedata.height}/>;
         } else {
-            image = <img src='/assets/video.png'/>
+            image = <img src='/assets/video.png'/>;
         }
         return (
             <div className='small-12 columns'>
