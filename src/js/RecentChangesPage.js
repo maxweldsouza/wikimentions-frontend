a8725b51f03ca8537b7425d39885af77c188c9e3
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-29 22:26:39 +0530

    fix tab alignment

--- src/js/RecentChangesPage.js
+++ src/js/RecentChangesPage.js
@@ -33,9 +33,8 @@ var RecentChangesPage = React.createClass({
                         <div className='row'>
                             <div className='small-12 columns'>
                                 <h1 className='page-title'>Recent Changes</h1>
-                                <div className='history-card'>
-                                    <div className="history-item small-12-columns">
-                                        <div className="row">
+                                <div className='card-container'>
+                                    <div className="card">
                                             <span className="small-8 columns"><a className="history-user" href="/users/2/maxweldsouza">maxweldsouza</a></span><span className="history-timestamp small-4 columns text-right">21 hours ago</span>
                                             <span className="small-12 columns">
                                                 <span className="history-added"><span className="ion-plus-circled" /></span>
@@ -43,7 +42,6 @@ var RecentChangesPage = React.createClass({
                                                         <a href="/pages/530/christopher-hitchens">Christopher Hitchens</a> mentioned <a href="/pages/108/richard-dawkins">Richard Dawkins</a> in <a href="/videos/531/hitchens-talks-at-google">Hitchens | Talks at Google</a>
                                                     </span>
                                                 </span>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
