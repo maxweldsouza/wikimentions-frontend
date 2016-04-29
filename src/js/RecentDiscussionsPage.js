78d7f8f3f43fb3252466d2c013e84ea05751a837
Maxwel Dsouza <maxellusionist@gmail.com>
2016-04-29 18:46:00 +0530

    css classes for cards and containers

--- src/js/RecentDiscussionsPage.js
+++ src/js/RecentDiscussionsPage.js
@@ -38,15 +38,17 @@ var RecentDiscussions = React.createClass({
                 <div className='row page-body align-center'>
                     <div className='small-12 large-8 columns'>
                         <h1 className='page-title'>Recent Discussions</h1>
-                        <div className='discuss-card row'>
-                            <div className='discuss-topic small-8 columns'>
-                                Richard Dawkins
-                            </div>
-                            <div className='discuss-posts small-2 column'>
-                                4 Posts
-                            </div>
-                            <div className='discuss-time small-2 column'>
-                                1 min ago
+                        <div className='card-container'>
+                            <div className='card'>
+                                <div className='discuss-topic small-8 columns'>
+                                    Richard Dawkins
+                                </div>
+                                <div className='discuss-posts small-2 column'>
+                                    4 Posts
+                                </div>
+                                <div className='discuss-time small-2 column'>
+                                    1 min ago
+                                </div>
                             </div>
                         </div>
                     </div>
