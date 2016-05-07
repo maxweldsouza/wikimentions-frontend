dcb834cd12c3a8f0723c64b0e73a6e1e1449fdb0
Maxwel Dsouza <maxellusionist@gmail.com>
2016-05-07 19:09:53 +0530

    change sidebar side

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -37,12 +37,6 @@ var Navbar = React.createClass({
         if (this.state.loggedin) {
             user = <ul className='menu'>
                 <li className='show-for-large'><Select name='mentioned' onSelectValue={this.onSelectSearchResult} placeholder={'Search'}/></li>
-                <li className='hide-for-large navbar-icon'>
-                    <i className='ion-android-search'/>
-                </li>
-                <li className='hide-for-large navbar-icon'>
-                    <i className='ion-android-person'/>
-                </li>
                 <li className='show-for-large'><a href={'/users/' + this.state.userid + '/' + this.state.username}>{this.state.username}</a></li>
                 <li className='show-for-large'>
                     <form action='/api/v1/logout' method='post'>
@@ -54,12 +48,6 @@ var Navbar = React.createClass({
         } else {
             user = <ul className='menu'>
                 <li className='show-for-large'><Select name='mentioned' onSelectValue={this.onSelectSearchResult} placeholder={'Search'}/></li>
-                <li className='hide-for-large navbar-icon'>
-                    <i className='ion-android-search'/>
-                </li>
-                <li className='hide-for-large navbar-icon'>
-                    <i className='ion-android-person'/>
-                </li>
                 <li><a href='/login'>Login</a></li>
                 <li><a href='/signup'>Signup</a></li>
             </ul>;
