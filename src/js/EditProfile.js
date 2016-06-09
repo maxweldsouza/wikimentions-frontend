2c5fad35fa14292f0da1d921f94974d54dcd3968
Maxwel Dsouza <maxellusionist@gmail.com>
2016-06-10 02:15:45 +0530

    fix rename passwords

--- src/js/EditProfile.js
+++ src/js/EditProfile.js
@@ -1,11 +1,16 @@
 var React = require('react');
 var cookies = require('browser-cookies');
+var SubmitButton = require('./SubmitButton');
+var requests = require('superagent');
+var Snackbar = require('./Snackbar');
 
 var EditProfile = React.createClass({
     getInitialState: function() {
         return {
+            submitting: false,
             old: '',
-            new: ''
+            new: '',
+            repeat: ''
         };
     },
     onChangeText (e) {
@@ -14,38 +19,41 @@ var EditProfile = React.createClass({
         this.setState(temp);
     },
     changePassword () {
-        requests
-        .post('/api/v1/changepassword')
-        .type('form')
-        .send({
-            old: this.state.old,
-            new: this.state.new,
-            _xsrf: cookies.get('_xsrf')
-        })
-        .end((err, res) => {
+        if (this.state.new !== this.state.repeat) {
+            Snackbar({message: "Password don't match"});
+        } else {
             this.setState({
-                submiting: false
+                submiting: true
             });
-            if (err && err.status) {
-                Snackbar({message: res.body.message});
-            } else {
-                Snackbar({message: 'Password changed'});
-                history.pushState(null, null, res.body.redirect);
-                Mentions.route(res.body.redirect);
-            }
-        });
+            requests
+            .post('/api/v1/changepassword')
+            .type('form')
+            .send({
+                old: this.state.old,
+                new: this.state.new,
+                _xsrf: cookies.get('_xsrf')
+            })
+            .end((err, res) => {
+                this.setState({
+                    submiting: false
+                });
+                if (err && err.status) {
+                    Snackbar({message: res.body.message});
+                } else {
+                    Snackbar({message: 'Password changed'});
+                }
+            });
+        }
     },
     render () {
         return (
             <div className='card'>
                 <div className='small-12 columns'>
                     <h2>Change Password</h2>
-                    <form action='/api/v1/changepassword' method='post'>
-                        <input type='password' name='old' placeholder='Old Password' onChange={this.onChangeText}/>
-                        <input type='password' name='new' placeholder='New Password' onChange={this.onChangeText}/>
-                        <input type='password' name='repeat' placeholder='Repeat Password' onChange={this.onChangeText}/>
-                        <button type='submit' className='button'>Change Password</button>
-                    </form>
+                    <input type='password' name='old' placeholder='Old Password' onChange={this.onChangeText}/>
+                    <input type='password' name='new' placeholder='New Password' onChange={this.onChangeText}/>
+                    <input type='password' name='repeat' placeholder='Repeat Password' onChange={this.onChangeText}/>
+                    <SubmitButton title='Save' submitting={this.state.submitting} onSubmit={this.changePassword}/>
                 </div>
             </div>
         );
