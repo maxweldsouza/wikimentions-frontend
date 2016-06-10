ec39694dd76c67269f9d36ff2056fafc6ae36caa
Maxwel Dsouza <maxellusionist@gmail.com>
2016-06-10 16:18:12 +0530

    ajax login

--- src/js/Login.js
+++ src/js/Login.js
@@ -5,6 +5,9 @@ var Navbar = require('./Navbar');
 var Login = require('./Login');
 var cookies = require('browser-cookies');
 var Xsrf = require('./Xsrf');
+var SubmitButton = require('./SubmitButton');
+var requests = require('superagent');
+var Snackbar = require('./Snackbar');
 
 var Login = React.createClass({
     statics: {
@@ -13,6 +16,43 @@ var Login = React.createClass({
                 api: []
             };
         }
+    },
+    getInitialState: function() {
+        return {
+            submitting: false,
+            username: '',
+            password: ''
+        };
+    },
+    onChangeText (e) {
+        var temp = {};
+        temp[e.target.name] = e.target.value;
+        this.setState(temp);
+    },
+    login () {
+        this.setState({
+            submiting: true
+        });
+        requests
+        .post('/api/v1/login')
+        .type('form')
+        .send({
+            username: this.state.username,
+            password: this.state.password,
+            _xsrf: cookies.get('_xsrf')
+        })
+        .end((err, res) => {
+            this.setState({
+                submiting: false
+            });
+            if (err && err.status) {
+                Snackbar({message: 'Login failed'});
+            } else {
+                Snackbar({message: 'Logged in'});
+                history.pushState(null, null, '/');
+                Mentions.route('/');
+            }
+        });
     },
     render () {
         return (
@@ -33,9 +73,9 @@ var Login = React.createClass({
                         <form action='/api/v1/login' method='post'>
                             <Xsrf/>
                             <h1>Login</h1>
-                            <input type='text' name='username' placeholder='Username' />
-                            <input type='password' name='password' placeholder='Password' />
-                            <button type='submit' className='success button'>Login</button>
+                            <input type='text' name='username' placeholder='Username' onChange={this.onChangeText}/>
+                            <input type='password' name='password' placeholder='Password' onChange={this.onChangeText}/>
+                            <SubmitButton title='Login' submitting={this.state.submitting} onSubmit={this.login}/>
                         </form>
                     </div>
                 </div>
