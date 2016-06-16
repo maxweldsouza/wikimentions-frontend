2935994004491d7df707afbf519397d7a3796351
Maxwel Dsouza <maxellusionist@gmail.com>
2016-06-16 20:26:18 +0530

    removed search from sidebar

--- src/js/MainComponent.js
+++ src/js/MainComponent.js
@@ -87,7 +87,6 @@ var MainComponent = React.createClass({
                     >
                     <a className="menu-item" href="/"><span className='ion-android-home menu-item-icon'/>Home</a>
                     <a className="menu-item" href="/create"><span className='ion-android-create menu-item-icon'/>Create Page</a>
-                    <a className="menu-item" href="/create"><span className='ion-android-search menu-item-icon'/>Search</a>
                     <a className="menu-item" href="/contribute"><span className='ion-ios-people menu-item-icon'/>Contribute</a>
                     <a className="menu-item" href="/blog"><span className='ion-document menu-item-icon'/>Blog</a>
                     <a className="menu-item" href="/blog/newpost"><span className='ion-compose menu-item-icon'/>New Blog Post</a>
