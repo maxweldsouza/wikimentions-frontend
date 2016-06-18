2d02e9191e3f02634c2e35da92d89db1bd900eb3
Maxwel Dsouza <maxellusionist@gmail.com>
2016-06-18 16:08:48 +0530

    prevent image flash

--- src/js/Image.js
+++ src/js/Image.js
@@ -4,9 +4,11 @@ var Lazy = require('react-lazy-load');
 var Image = React.createClass({
     render () {
         return (
-            <Lazy height={this.props.height}>
-                <img className={this.props.className} src={'/api/v1/static/images/' + this.props.md5 + '-' + this.props.width + '-' + this.props.height + '.jpg'} width={this.props.width} height={this.props.height} alt=""/>
-            </Lazy>
+            <div style={{'width': this.props.width, 'height': this.props.height}} >
+                <Lazy height={this.props.height}>
+                    <img className={this.props.className} src={'/api/v1/static/images/' + this.props.md5 + '-' + this.props.width + '-' + this.props.height + '.jpg'} width={this.props.width} height={this.props.height} alt=""/>
+                </Lazy>
+            </div>
         );
     }
 });
