137a54c6ef4cdc883fa6ad71f1c82850b440fb57
Maxwel Dsouza <maxellusionist@gmail.com>
2016-07-08 17:08:20 +0530

    new format for history

--- src/js/HistoryItem.js
+++ src/js/HistoryItem.js
@@ -5,60 +5,99 @@ var Markdown = require('./Markdown');
 
 var HistoryCard = React.createClass({
     render () {
-        var added = this.props.deleted === 1 ? <span className="ion-minus-circled"></span> : <span className="ion-plus-circled"></span>;
+        var added = this.props.deleted === 1 ? 'Deleted' : 'Added';
         var item;
         if (this.props.entry && this.props.entrytype === 'video_author') {
-            item = <span>
-                <Link
-                    id={this.props.entry.source.id}
-                    slug={this.props.entry.source.props.slug}
-                    type={this.props.entry.source.props.type}>
-                    {this.props.entry.source.title}
-                </Link> as author to video <Link
-                    id={this.props.entry.destination.id}
-                    slug={this.props.entry.destination.props.slug}
-                    type={this.props.entry.destination.props.type}>
-                    {this.props.entry.destination.title}
-                </Link>
+            item = <span className='row'>
+                <span className='small-12 columns'>
+                    {added} Author
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Author:</strong> <Link
+                        id={this.props.entry.source.id}
+                        slug={this.props.entry.source.props.slug}
+                        type={this.props.entry.source.props.type}>
+                        {this.props.entry.source.props.title}
+                    </Link>
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Video:</strong> <Link
+                        id={this.props.entry.destination.id}
+                        slug={this.props.entry.destination.props.slug}
+                        type={this.props.entry.destination.props.type}>
+                        {this.props.entry.destination.props.title}
+                    </Link>
+                </span>
             </span>;
         } else if (this.props.entry && this.props.entrytype === 'book_author') {
-            item = <span>
-                <Link
-                    id={this.props.entry.source.id}
-                    slug={this.props.entry.source.props.slug}
-                    type={this.props.entry.source.props.type}>{this.props.entry.source.props.title}
-                </Link> as author to book <Link
-                    id={this.props.entry.destination.id}
-                    slug={this.props.entry.destination.props.slug}
-                    type={this.props.entry.destination.props.type}>
-                    {this.props.entry.destination.props.title}
-                </Link>
+            item = <span className='row'>
+                <span className='small-12 columns'>
+                    {added} Author
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Book:</strong> <Link
+                        id={this.props.entry.source.id}
+                        slug={this.props.entry.source.props.slug}
+                        type={this.props.entry.source.props.type}>{this.props.entry.source.props.title}
+                    </Link>
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Author:</strong> <Link
+                        id={this.props.entry.destination.id}
+                        slug={this.props.entry.destination.props.slug}
+                        type={this.props.entry.destination.props.type}>
+                        {this.props.entry.destination.props.title}
+                    </Link>
+                </span>
             </span>;
         } else if (this.props.entry && this.props.entrytype === 'prop') {
-            item = <span>
-                set {this.props.entry.key} to {this.props.entry.value} for <Link
-                    id={this.props.entry.obj.id}
-                    slug={this.props.entry.obj.props.slug}
-                    type={this.props.entry.obj.props.type}>{this.props.entry.obj.props.title}</Link>
+            item = <span className='row'>
+                <span className='small-12 columns'>
+                    {added} Property
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Page:</strong> <Link
+                        id={this.props.entry.obj.id}
+                        slug={this.props.entry.obj.props.slug}
+                        type={this.props.entry.obj.props.type}>{this.props.entry.obj.props.title}
+                    </Link>
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Name:</strong> {this.props.entry.key}
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Value:</strong> {this.props.entry.value}
+                </span>
             </span>;
         } else if (this.props.entry && this.props.entrytype === 'mention') {
-            item = <span>
-                <Link
-                    id={this.props.entry.mentioned_by.id}
-                    slug={this.props.entry.mentioned_by.props.slug}
-                    type={this.props.entry.mentioned_by.props.type}>
-                    {this.props.entry.mentioned_by.props.title}
-                </Link> mentioned <Link
-                    id={this.props.entry.mentioned.id}
-                    slug={this.props.entry.mentioned.props.slug}
-                    type={this.props.entry.mentioned.props.type}>
-                    {this.props.entry.mentioned.props.title}
-                </Link> in <Link
-                    id={this.props.entry.mentioned_in.id}
-                    slug={this.props.entry.mentioned_in.props.slug}
-                    type={this.props.entry.mentioned_in.props.type}>
-                    {this.props.entry.mentioned_in.props.title}
-                </Link>
+            item = <span className='row'>
+                <span className='small-12 columns'>
+                    {added} Mention
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Mentioned by:</strong> <Link
+                        id={this.props.entry.mentioned_by.id}
+                        slug={this.props.entry.mentioned_by.props.slug}
+                        type={this.props.entry.mentioned_by.props.type}>
+                        {this.props.entry.mentioned_by.props.title}
+                    </Link>
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Mentioned:</strong> <Link
+                        id={this.props.entry.mentioned.id}
+                        slug={this.props.entry.mentioned.props.slug}
+                        type={this.props.entry.mentioned.props.type}>
+                        {this.props.entry.mentioned.props.title}
+                    </Link>
+                </span>
+                <span className='small-12 columns'>
+                    <strong>Mentioned In</strong> <Link
+                        id={this.props.entry.mentioned_in.id}
+                        slug={this.props.entry.mentioned_in.props.slug}
+                        type={this.props.entry.mentioned_in.props.type}>
+                        {this.props.entry.mentioned_in.props.title}
+                    </Link>
+                </span>
             </span>;
         } else if (this.props.entry && this.props.entrytype === 'image') {
             var name = this.props.entry.md5 + '-' + this.props.entry.width + '-' + this.props.entry.height + '.jpg';
@@ -73,7 +112,7 @@ var HistoryCard = React.createClass({
                     </span>
                     <span className='small-4 columns text-right'><Time timestamp={this.props.timestamp} type='ago'/></span>
                     <span className='small-12 columns'>
-                        <span>{added}</span> {item}
+                        {item}
                     </span>
             </div>
         );
