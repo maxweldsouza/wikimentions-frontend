c0c102163bbd277bdd60df7bff89b8de57b33334
Maxwel Dsouza <maxellusionist@gmail.com>
2016-07-11 11:52:51 +0530

    show stores by country

--- src/js/Affiliate.js
+++ src/js/Affiliate.js
@@ -1,16 +1,18 @@
 var React = require('react');
 var config = require('./config');
+var store = require('store');
 
 var Affiliate = React.createClass({
     render () {
+        var country = store.get('country');
         return (
             <div>
-                <div>
+                {country === 'US' ? <div>
                     <a target='_blank' href={'http://www.amazon.com/gp/search?keywords=' + this.props.isbn + '&index=books&linkCode=qs&tag=' + config['affiliate']['amazon.com']}>amazon.com <span className='ion-android-open'/></a>
-                </div>
-                <div>
+                </div> : null}
+                {country === 'IN' ? <div>
                     <a target='_blank' href={'http://www.amazon.in/gp/search?keywords=' + this.props.isbn + '&index=books&linkCode=qs&tag=' + config['affiliate']['amazon.com']}>amazon.in <span className='ion-android-open'/></a>
-                </div>
+                </div> : null}
             </div>
         );
     }
