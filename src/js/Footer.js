c70ee1e781ee5da81e2b228b63d96ab1f87ebe25
Maxwel Dsouza <maxellusionist@gmail.com>
2016-07-11 16:18:34 +0530

    fix footer links

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -8,17 +8,17 @@ var Footer = React.createClass({
                         <hr/>
                     </div>
                     <div className='small-12 large-6 columns'>
-                        <ul className="menu">
-                            <li><a href="#">Create</a></li>
-                            <li><a href="#">Contribute</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact</a></li>
+                        <ul className='menu'>
+                            <li><a href='/create'>Create</a></li>
+                            <li><a href='/contribute'>Contribute</a></li>
+                            <li><a href='/blog'>Blog</a></li>
+                            <li><a href='/contact'>Contact</a></li>
                         </ul>
                     </div>
                     <div className='small-12 large-6 columns'>
-                        <ul className="menu align-right">
-                            <li><a href="/terms-of-use">Terms of Use</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
+                        <ul className='menu align-right'>
+                            <li><a href='/terms-of-use'>Terms of Use</a></li>
+                            <li><a href='/privacy-policy'>Privacy Policy</a></li>
                         </ul>
                     </div>
                 </div>
