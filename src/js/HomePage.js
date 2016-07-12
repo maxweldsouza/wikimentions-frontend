46e979795e6192f1189f4756b8f23e06d9799b80
Maxwel Dsouza <maxellusionist@gmail.com>
2016-07-12 18:24:16 +0530

    bold wikimentions

--- src/js/HomePage.js
+++ src/js/HomePage.js
@@ -61,7 +61,7 @@ var HomePage = React.createClass({
                 <div className='row page-body align-center'>
                     <div className='small-12 xlarge-4 columns'>
                         <div className='callout warning'>
-                            <h2>WikiMentions</h2>
+                            <h2><strong>WikiMentions</strong></h2>
                             Discover people and their work based on their mentions.
                         </div>
                         <div className='callout show-for-xlarge'>
