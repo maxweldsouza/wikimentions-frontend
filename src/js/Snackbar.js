e10d1b9107636eede015508f0b350c61e691ba2b
Maxwel Dsouza <maxellusionist@gmail.com>
2016-07-17 18:35:07 +0530

    fix snackbar behavior

--- src/js/Snackbar.js
+++ src/js/Snackbar.js
@@ -1,37 +1,53 @@
 var $ = require('jquery');
 var _ = require('lodash');
 
-var Snackbar = function (options) {
-    var defaults = {
-        duration: 3000,
-        message: '',
-        onAction: null,
-        showActionButton: false,
-        actionText: ''
+var Snackbar = function () {
+    var visible = false;
+    $('body').append('<div class="snackbar" id="snackbar"><span class="snackbar-message" id="snackbar-message"></span></div>');
+    var i = 0;
+    return function (options) {
+        var defaults = {
+            animationDuration: 200,
+            duration: 3000,
+            message: '',
+        };
+        defaults = _.extend(defaults, options);
+
+        // Validation
+        if (defaults.message.length > 20) {
+            console.warn('Snackbar message is too long');
+        }
+
+        var message = '<span class="snackbar-message">' + defaults.message + '</span>';
+        i = i + 1;
+        var j = i;
+        if (visible) {
+            $('#snackbar').removeClass('showing');
+            visible = false;
+            setTimeout(function () {
+                $('.snackbar-message').replaceWith(message);
+                $('#snackbar').addClass('showing');
+                visible = true;
+                setTimeout(function () {
+                    if (i == j) {
+                        $('#snackbar').removeClass('showing');
+                        visible = false;
+                    }
+                }, defaults.duration);
+            }, defaults.animationDuration);
+        } else {
+            $('.snackbar-message').replaceWith(message);
+            $('#snackbar').addClass('showing');
+            visible = true;
+            setTimeout(function () {
+                if (i == j) {
+                    $('#snackbar').removeClass('showing');
+                    visible = false;
+                }
+            }, defaults.duration);
+        }
     };
-    defaults = _.extend(defaults, options);
+}();
 
-    // Validation
-    if (defaults.message.length > 20) {
-        console.warn('Snackbar message is too long');
-    }
-
-    // DOM Elements
-    var action = defaults.showActionButton ? '<span class="snackbar-action">' + defaults.actionText + '</span>' : '';
-    var message = '<span class="snackbar-message">' + defaults.message + '</span>';
-    var snack = '<div class="snackbar" id="snackbar">' + message + action + '</div>';
-
-    if ($('#snackbar').length) {
-        $('#snackbar').replaceWith(snack);
-    } else {
-        $('body').append(snack);
-    }
-    setTimeout(function () {
-        $('#snackbar').addClass('showing');
-    }, 0);
-    setTimeout(function () {
-        $('#snackbar').removeClass('showing');
-    }, defaults.duration);
-};
 
 module.exports = Snackbar;
