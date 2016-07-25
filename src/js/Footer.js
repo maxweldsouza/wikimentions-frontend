fcf1c8888844742c6d24f7e1af1579359f559cd1
Maxwel Dsouza <maxellusionist@gmail.com>
2016-07-25 20:16:04 +0530

    warning text in footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -29,7 +29,6 @@ var Footer = React.createClass({
                                     <li><a href='/create'>Create Page</a></li>
                                     <li><a href='/recent-changes'>Recent Changes</a></li>
                                     <li><a href='/recent-discussions'>Recent Discussions</a></li>
-                                    <li><a href='/blog'>Blog</a></li>
                                 </ul>
                             </div>
                             <div className='small-12 large-3 columns'>
@@ -37,8 +36,6 @@ var Footer = React.createClass({
                                     <li className="menu-text">Help</li>
                                     <li><a href='/about-us'>About Us</a></li>
                                     <li><a href='/guidelines'>Guidelines</a></li>
-                                    <li><a href='/terms-of-use'>Terms of Use</a></li>
-                                    <li><a href='/privacy-policy'>Privacy Policy</a></li>
                                 </ul>
                             </div>
                             <div className='small-12 large-3 columns'>
@@ -46,9 +43,13 @@ var Footer = React.createClass({
                                     <li className="menu-text">Social</li>
                                     <li><a href={config.social.facebook}>Facebook</a></li>
                                     <li><a href={config.social.twitter}>Twitter</a></li>
+                                    <li><a href='/blog'>Blog</a></li>
                                 </ul>
                             </div>
                         </div>
+                    </div>
+                    <div className='small-12 columns text-center' style={{'fontSize': '12px', marginTop: 30}}>
+                        By using {config.name} you agree to our <a href='/terms-of-use'>Terms of Use</a> and <a href='/privacy-policy'>Privacy Policy</a> which were <strong>last updated</strong> on <strong>1st August 2016.</strong>
                     </div>
                 </div>
             </div>
