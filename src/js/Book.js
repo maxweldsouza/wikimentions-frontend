5efb7f5c72632c57f82cb8e811ec654f192d4eec
Maxwel Dsouza <maxellusionist@gmail.com>
2016-08-05 21:12:50 +0530

    books dont need isbn

--- src/js/Book.js
+++ src/js/Book.js
@@ -6,10 +6,6 @@ var Thumbnail = require('./Thumbnail');
 
 var Book = React.createClass({
     render () {
-        var message;
-        if (!(this.props.isbn || this.props.isbn13)) {
-            message = <span className="ion-alert-circled"></span>;
-        }
         return (
             <div className='small-12 medium-6 large-4 columns text-center'>
                 <a href={'/books/' + this.props.id + '/' + this.props.slug}>
@@ -22,7 +18,7 @@ var Book = React.createClass({
                         displayHeight={200} />
                 </div>
                 <div className='card-title'>
-                    {this.props.title} {message}
+                    {this.props.title}
                 </div>
                 </a>
             </div>
