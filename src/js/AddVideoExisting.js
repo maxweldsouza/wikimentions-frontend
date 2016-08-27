21abc6bab6cdbe16db73f6e9d6e2aedf36bf28ab
Maxwel Dsouza <maxellusionist@gmail.com>
2016-08-27 19:47:30 +0530

    fix add video existing

--- src/js/AddVideoExisting.js
+++ src/js/AddVideoExisting.js
@@ -41,33 +41,35 @@ var AddVideo = React.createClass({
         return valid;
     },
     onSubmit () {
-        this.setState({
-            submitting: true
-        });
-        requests
-        .post('/api/v1/thing/' + this.props.id + '/videos')
-        .type('form')
-        .send({
-            video_id: this.state.video_id,
-            _xsrf: cookies.get('_xsrf')
-        })
-        .end((err, res) => {
+        if (this.validateForm()) {
             this.setState({
-                submitting: false
+                submitting: true
             });
-            if (err && err.status) {
+            requests
+            .post('/api/v1/thing/' + this.props.id + '/videos')
+            .type('form')
+            .send({
+                video_id: this.state.video_id,
+                _xsrf: cookies.get('_xsrf')
+            })
+            .end((err, res) => {
                 this.setState({
-                    formMessage: res.body.message
+                    submitting: false
                 });
-            } else {
-                this.setState({
-                    formMessage: ''
-                });
-                Snackbar({message: 'Video added'});
-                history.pushState(null, null, window.location.pathname + window.location.search);
-                Mentions.route(window.location.pathname + window.location.search);
-            }
-        });
+                if (err && err.status) {
+                    this.setState({
+                        formMessage: res.body.message
+                    });
+                } else {
+                    this.setState({
+                        formMessage: ''
+                    });
+                    Snackbar({message: 'Video added'});
+                    history.pushState(null, null, window.location.pathname + window.location.search);
+                    Mentions.route(window.location.pathname + window.location.search);
+                }
+            });
+        }
     },
     render () {
         var id = this.props.id;
@@ -76,7 +78,12 @@ var AddVideo = React.createClass({
                 {this.state.formMessage}
             </div> : null}
             Search for video
-            <Select name='video_id' onSelectValue={this.onSelect} types={['video']}/>
+            <Select
+                name='video_id'
+                onSelectValue={this.onSelect}
+                types={['video']}
+                valid={this.state.videoValid}
+                message={this.state.videoMessage} />
             <IpWarning loggedin={this.props.loggedin}/>
             <SubmitButton title='Add' className='button primary float-right' submitting={this.state.submitting} onSubmit={this.onSubmit}/>
         </div>;
