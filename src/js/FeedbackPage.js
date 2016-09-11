6791372769e9fb7e74e67ddbd52c60a8c32a39cd
Maxwel Dsouza <maxellusionist@gmail.com>
2016-09-11 17:28:16 +0530

    fix snackbar not defined

--- src/js/FeedbackPage.js
+++ src/js/FeedbackPage.js
@@ -1,11 +1,12 @@
-var React = require('react');
-var Helmet = require('react-helmet');
-var Navbar = require('./Navbar');
-var config = require('./config');
-var Markdown = require('./Markdown');
-var Time = require('./Time');
 var AdminOnly = require('./AdminOnly');
+var config = require('./config');
+var Helmet = require('react-helmet');
+var Markdown = require('./Markdown');
+var Navbar = require('./Navbar');
+var React = require('react');
 var requests = require('superagent');
+var Time = require('./Time');
+var Snackbar = require('./Snackbar');
 
 var FeedbackPage = React.createClass({
     statics: {
