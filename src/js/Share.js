60d46a5c8464903440dd32a3a695d5bfe4b02f9a
Maxwel Dsouza <maxellusionist@gmail.com>
2017-03-30 21:05:24 +0530

    Cleanup Share

--- src/js/Share.js
+++ src/js/Share.js
@@ -1,43 +1,33 @@
 import React from 'react';
 import config from './config';
-import autoBind from 'react-autobind';
 
-class Share extends React.Component {
-    constructor(props) {
-        super(props);
-        autoBind(this);
-        this.state = {
-            open: false
-        };
-    }
-    render() {
-        return (
-            <div>
-                <div className="button-group small">
-                    <a
-                        role="button"
-                        href={
-                            `http://www.facebook.com/sharer/sharer.php?u=${config.url}${this.props.path}&title=${this.props.title}`
-                        }
-                        className="button"
-                        style={{ background: '#3b5998' }}
-                    >
-                        <span className="ion-social-facebook" /> Share
-                    </a>
-                    <a
-                        role="button"
-                        href={
-                            `http://twitter.com/intent/tweet?status=${this.props.title}+${config.url}${this.props.path}`
-                        }
-                        className="button"
-                        style={{ background: '#55aace' }}
-                    >
-                        <span className="ion-social-twitter" /> Tweet
-                    </a>
-                </div>
+const Share = ({ title, path }) => {
+    return (
+        <div>
+            <div className="button-group small">
+                <a
+                    role="button"
+                    href={
+                        `http://www.facebook.com/sharer/sharer.php?u=${config.url}${path}&title=${title}`
+                    }
+                    className="button"
+                    style={{ background: '#3b5998' }}
+                >
+                    <span className="ion-social-facebook" /> Share
+                </a>
+                <a
+                    role="button"
+                    href={
+                        `http://twitter.com/intent/tweet?status=${title}+${config.url}${path}`
+                    }
+                    className="button"
+                    style={{ background: '#55aace' }}
+                >
+                    <span className="ion-social-twitter" /> Tweet
+                </a>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Share;
